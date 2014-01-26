e6c60c27103eed1938bb2ebd7ba884e06d59dbc7
DoppelkopfProjekt <deaceade@secretemail.de>
2014-01-26 20:42:17 +0100

    TStich vorerst fertig

--- DoppelkopfServer/mTStich.pas
+++ DoppelkopfServer/mTStich.pas
@@ -2,7 +2,7 @@ unit mTStich;
 
 interface
 
-uses Sysutils, classes, mTSpieler, mTKarte;
+uses Sysutils, classes, mTSpieler, mTKarte, Dialogs;
 
 type
 TStich = class
@@ -12,20 +12,24 @@ FSpielerListe: TList;
 FAktuellerStich: TList;
 FAktuellerSieger: TSpieler;
 FAktuellBesteKarte: TKarte;
+FAktuellerSpieler: TSpieler;
+FNummer: Integer;
 public
 property AktuellerSieger: TSpieler read FAktuellerSieger;
 procedure AddSpieler(pSpieler: TSpieler);
-procedure LegeKarte(pKarte: TKarte, pLegenderSpieler: TSpieler);
+procedure LegeKarte(pKarte: TKarte; pLegenderSpieler: TSpieler);
+constructor Create(pNummer: Integer);
 end;
 
 implementation
 
-constructor TStich.Create;
+constructor TStich.Create(pNummer: Integer);
 begin
   FAktuellerSieger := nil;
   FAktuellBesteKarte := nil;
   FAktuellerStich := TList.Create;
   FSpielerListe := TList.Create;
+  FNummer := pNummer;
 end;
 
 procedure TStich.AddSpieler(pSpieler: TSpieler);
@@ -37,17 +41,36 @@ if FSpielerListe.count >= 4 then ShowMessage('Zu viele Spieler')
   end;
 end;
 
-procedure TStich.LegeKarte(pKarte: TKarte, pLegenderSpieler: TSpieler)
+procedure TStich.LegeKarte(pKarte: TKarte; pLegenderSpieler: TSpieler);
+var Ergebnis: dkErgebnis;
 begin
-if FAktuellerStich.Count >= 4 then ShowMessage('5 Karten auf einem Stich!?!);
-FAktuellerStich.add(pKarte: TKarte);
-if not FAktuellerSpieler[FAktuellerStich.count-1] = pLegenderSpieler then ShowMessage('Der falsche Spieler legt die Karte!?!');
-if FAktuellerStich.count = 1 then
-begin
-  FAktuellerSieger := FSpielerListe[0];
-  FAktuellBesteKarte := pKarte;
-end;
+  if FSpielerListe.Count = 4 then ShowMessage('Keine 4 Spieler zum Stich hinzugefügt!');
+  if FAktuellerStich.Count >= 4 then ShowMessage('5 Karten auf einem Stich!?!');
 
+  FAktuellerStich.add(pKarte);
+  if not (FSpielerListe[FAktuellerStich.Count-1] = pLegenderSpieler) then ShowMessage('Der falsche Spieler legt die Karte!?!');
+  if FAktuellerStich.count = 1 then
+  begin
+    FAktuellerSieger := FSpielerListe[0];
+    FAktuellBesteKarte := pKarte;
+  end else
+  begin
+    Ergebnis := FAktuellBesteKarte.kannKarteMichStechen(pKarte);
+    if Ergebnis = staerker then
+    begin
+      FAktuellerSieger := pLegenderSpieler;
+      FAktuellBesteKarte := pKarte;
+    end;
+    if Ergebnis = gleich then
+    begin
+      if (pKarte.Code = 'HE10') and not (FNummer = 10) then
+      begin
+        FAktuellerSieger := pLegenderSpieler;
+        FAktuellBesteKarte := pKarte;
+       end;
+     //Ansonsten gewinnt die zuerst gelegte Karte
+    end;
+  end;
 end;
 
 end.
