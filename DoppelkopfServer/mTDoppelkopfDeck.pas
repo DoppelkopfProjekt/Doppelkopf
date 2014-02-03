71182a5e3f1cebbcae5808bcf0fb452eba81a6de
DoppelkopfProjekt <deaceade@secretemail.de>
2014-02-03 12:10:48 +0100

    Karten werden jetzt erstellt

--- DoppelkopfServer/mTDoppelkopfDeck.pas
+++ DoppelkopfServer/mTDoppelkopfDeck.pas
@@ -20,8 +20,50 @@ implementation
 
 constructor TDoppelkopfDeck.Create;
 begin
+  FKarten.Add(TKarte.create('KA10'));
+  FKarten.Add(TKarte.create('KAB'));
+  FKarten.Add(TKarte.create('KAD'));
   FKarten.Add(TKarte.create('KAK'));
-  //So müssen alle 40 Karten erstellt werden...
+  FKarten.Add(TKarte.create('KAA'));
+  FKarten.Add(TKarte.create('HE10'));
+  FKarten.Add(TKarte.create('HEB'));
+  FKarten.Add(TKarte.create('HED'));
+  FKarten.Add(TKarte.create('HEK'));
+  FKarten.Add(TKarte.create('HEA'));
+  FKarten.Add(TKarte.create('PI10'));
+  FKarten.Add(TKarte.create('PIB'));
+  FKarten.Add(TKarte.create('PID'));
+  FKarten.Add(TKarte.create('PIK'));
+  FKarten.Add(TKarte.create('PIA'));
+  FKarten.Add(TKarte.create('KR10'));
+  FKarten.Add(TKarte.create('KRB'));
+  FKarten.Add(TKarte.create('KRD'));
+  FKarten.Add(TKarte.create('KRK'));
+  FKarten.Add(TKarte.create('KRA'));
+  //ab hier die Karten nochmal
+  FKarten.Add(TKarte.create('KA10'));
+  FKarten.Add(TKarte.create('KAB'));
+  FKarten.Add(TKarte.create('KAD'));
+  FKarten.Add(TKarte.create('KAK'));
+  FKarten.Add(TKarte.create('KAA'));
+  FKarten.Add(TKarte.create('HE10'));
+  FKarten.Add(TKarte.create('HEB'));
+  FKarten.Add(TKarte.create('HED'));
+  FKarten.Add(TKarte.create('HEK'));
+  FKarten.Add(TKarte.create('HEA'));
+  FKarten.Add(TKarte.create('PI10'));
+  FKarten.Add(TKarte.create('PIB'));
+  FKarten.Add(TKarte.create('PID'));
+  FKarten.Add(TKarte.create('PIK'));
+  FKarten.Add(TKarte.create('PIA'));
+  FKarten.Add(TKarte.create('KR10'));
+  FKarten.Add(TKarte.create('KRB'));
+  FKarten.Add(TKarte.create('KRD'));
+  FKarten.Add(TKarte.create('KRK'));
+  FKarten.Add(TKarte.create('KRA'));
+ 
+  
+  //So mï¿½ssen alle 40 Karten erstellt werden...
 end;
 
 function TDoppelkopfDeck.getRandomCard: TKarte;
